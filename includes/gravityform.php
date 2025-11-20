08aba4d43ea7d2be7a245cd1dfeea4aa6b9f9c99
Sajjad Hashemian <hi@hashemian.me>
2025-11-20 15:45:24 +0330

    add gravityform support

---
+++ includes/gravityform.php
@@ -0,0 +1,81 @@
+<?php
+
+if (!defined('ABSPATH')) {
+  exit;
+}
+
+function stopchurn_gform_after_submission($entry, $form) {
+  $user = [
+    "email" => '',
+    "phone" => '',
+    "firstName" => '',
+    "lastName" => '',
+  ];
+  $country_code = '';
+
+  foreach ( $form['fields'] as $field ) {
+    switch ($field->type) {
+      case 'name':
+        $user['firstName'] = $field->id . '.3';
+        $user['lastName'] = $field->id . '.6';
+        break;
+      case 'email':
+      case 'phone':
+        $user[$field->type] = (string) $field->id;
+        break;
+    }
+
+    if ($field->label === 'Country Code' || $field->adminLabel === 'Country Code' || $field->autocompleteAttribute === 'tel-country-code') {
+      $country_code = $field->id;
+    }
+
+    if (
+      !$user['firstName'] &&
+      (strpos($field->label, 'First Name') !== false || strpos($field->adminLabel, 'First Name') !== false || $field->autocompleteAttribute === 'given-name')
+    ) {
+      $user['firstName'] = $field->id;
+    }
+
+    if (
+      !$user['lastName'] &&
+      (strpos($field->label, 'Last Name') !== false || strpos($field->adminLabel, 'Last Name') !== false || $field->autocompleteAttribute === 'family-name')
+    ) {
+      $user['lastName'] = $field->id;
+    }
+  }
+
+  foreach($user as $k => $id) {
+    $user[$k] = $id ? rgar( $entry, $id ) : '';
+  }
+
+  if ($country_code && $user['phone']) {
+    $user['phone'] = rgar( $entry, $country_code ) . $user['phone'];
+  }
+
+  if (!$user['email']) {
+    return;
+  }
+
+  $user['id'] = $user['email'];
+
+  stopchurn_send_user_update($user);
+
+  stopchurn_send_user_update(
+    [
+      "id" => $form['id'] . '_' . $user['email'] . '_' . uniqid(),
+      "userId" => $user['email'],
+      "formId" => $form['id'],
+      "formTitle" => $form['title'],
+    ],
+    'formSubmission',
+    'insert',
+    true,
+  );
+
+  stopchurn_send_user_event(
+    $user['email'],
+    'SUBMISSION',
+    $form['title'],
+  );
+}
+add_action('gform_after_submission', 'stopchurn_gform_after_submission', 10, 2);
--- stopchurn.php
+++ stopchurn.php
@@ -17,3 +17,4 @@ define('STOPCHURN_PLUGIN_DIR', plugin_dir_path(__FILE__));
 require_once STOPCHURN_PLUGIN_DIR . 'includes/settings.php';
 require_once STOPCHURN_PLUGIN_DIR . 'includes/init.php';
 require_once STOPCHURN_PLUGIN_DIR . 'includes/contactform7.php';
+require_once STOPCHURN_PLUGIN_DIR . 'includes/gravityform.php';
