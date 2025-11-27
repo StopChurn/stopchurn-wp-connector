884e4f5a4094da279989ad2e0fc1a319aee30310
Sajjad Hashemian <hi@hashemian.me>
2025-11-27 09:02:43 +0330

    rename file

--- stopchurn.php
+++ stopchurn.php
@@ -15,7 +15,7 @@ if (!defined('ABSPATH')) {
 define('STOPCHURN_PLUGIN_DIR', plugin_dir_path(__FILE__));
 
 require_once STOPCHURN_PLUGIN_DIR . 'includes/settings.php';
-require_once STOPCHURN_PLUGIN_DIR . 'includes/init.php';
+require_once STOPCHURN_PLUGIN_DIR . 'includes/functions.php';
 require_once STOPCHURN_PLUGIN_DIR . 'includes/contactform7.php';
 require_once STOPCHURN_PLUGIN_DIR . 'includes/gravityform.php';
 require_once STOPCHURN_PLUGIN_DIR . 'includes/lifterlms.php';
